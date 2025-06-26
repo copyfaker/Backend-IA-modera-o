b4fc0dd3b2b6322ca9a80cf9b4ebb7b358685df1
copyfaker <copyfaker8@gmail.com>
2025-06-25 23:13:43 -0300

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import moderateRoute from "./routes/moderate.js";
+import moderateRoute from "./moderate.js";
 
 dotenv.config();
 
