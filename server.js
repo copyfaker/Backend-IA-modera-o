a889e2d6c46f437ec5314523ff5a388627d09182
copyfaker <copyfaker8@gmail.com>
2025-06-25 23:07:33 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import moderateRoute from "./routes/moderate.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/moderate", moderateRoute);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
